958d8d125dcf6d6c7f9b93f56c58cedb01c6bd2d
dirty6613 <cmacchi@comcast.net>
2014-04-02 18:53:12 -0400

    Updated Goal 1 Strings

--- Wk1/goal1_basics/js/main.js
+++ Wk1/goal1_basics/js/main.js
@@ -66,24 +66,35 @@ console.log('------ variables ----------');
 
 
     //example 4: declaring multiple variables at once w/ same value
-	var a = b = c = d = 10;
-	console.log ("a: ", a  + " / b: ", b  + " / c: ", c  + " / d: ", d);
+	//var a = b = c = d = 10;
+	//console.log ("a: ", a  + " / b: ", b  + " / c: ", c  + " / d: ", d);
 
 
 // string (definition, concatenation, escape character)
 console.log('------ strings ----------');
-
-    //basic string
-
+	var name = "James Bond";
+	console.log(name);
+    
+	//basic string
+	var testString = "He's in PWA1";
+	console.log("Example 1: ", testString);
 
     //string w/ escape characters " \ "
+	var testString = "I need some \"quotes\" to be here.";
+	console.log ("Example 2: ", testString);
+	
+	var testString = 'He\'s in PWA1';
+	console.log("Example 3: ", testString);
 
 
     //string w/ escape characters and mixed single/double quotes
-
+	var testString = 'he\'s in ' + '"PWA1"';
+	console.log("Example 4: ", testString);
 
     //this example shows that all the "phase" vars above were overwritten
-
+	var num = 3;
+	var testString = 'he\'s in PWA1 ' + num;
+	console.log("Example 5: ", testString);
 
 
 // numbers (definition, concatenation +, math, numbers & strings)
