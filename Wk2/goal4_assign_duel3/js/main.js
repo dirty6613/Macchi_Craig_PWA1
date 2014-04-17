ec742b2b6120e3761b6742f4257f4999c2fb7c19
dirty6613 <cmacchi@comcast.net>
2014-04-17 01:54:14 -0400

    Math working, and html updating.  Still need to declare winner and have end to game.

--- Wk2/goal4_assign_duel3/js/main.js
+++ Wk2/goal4_assign_duel3/js/main.js
@@ -3,7 +3,7 @@
 // Name: Craig Macchi
 // Date: 4/16/14
 // Assignment: WEEK 3 Assignment: The Duel - Part III
-(function(){
+//(function(){
 	
 	window.onload = init;
 	
@@ -14,13 +14,12 @@ console.log(" ** FIGHT ** ");
 //Define doms
 var fighter1_txt = document.getElementById("kabal").getElementsByTagName("p")[0];
 var fighter2_txt = document.getElementById("kratos").getElementsByTagName("p")[0];
-console.log(fighter2_txt);
-var round_txt = document.getElementByTagName("h4");
+var round_txt = document.getElementsByTagName("h4")[0];
 var button = document.getElementById("fight_btn");
-
+console.log(button);
 //Define Variables in an array
-var playerOne = ["Spiderman", 100, 50];
-var playerTwo = ["Superman", 100, 50];
+//var playerOne = ["Spiderman", 100, 50];
+//var playerTwo = ["Superman", 100, 50];
 
 // Define Variables in an Object
 var fighters = [
@@ -43,20 +42,41 @@ button.addEventListener("click",fight, false);
 // Fight Function
 function fight(){
 	
+	console.log("hh");
 	//Start match - display name/health
 	fighter1_txt.innerHTML = fighters[0].name + ":" + fighters[0].health;
-	
-	
+	fighter2_txt.innerHTML = fighters[1].name + ":" + fighters[1].health;
+			//random formula to generate damage
+		var minDam1 = fighters[0].damage * .5;
+		var minDam2 = fighters[1].damage * .5;
+		var f1 = Math.floor(Math.random() * fighters[0].damage + minDam1);
+		var f2 = Math.floor(Math.random() * fighters[1].damage + minDam2);
+	//generate damage on players
+		fighters[0].health = fighters[0].health - f1;
+		fighters[1].health = fighters[1].health - f2;
+		
+		//Update health of players
+		console.log("Round " + rd + ":" + fighters[0].name + ": " +fighters[0].health + " vs " + fighters[1].name + ": " + fighters[1].health + ".");
+		
+		//check if either player has died
+		var results = winnerCheck();
+		if (results === "game on"){
+		//alert(playerOne [0] + ": " +fighters[0] + " *ROUND " + rd + " OVER* " + playerTwo[0] + ": " + fighters[1] + ".");
+		}else{
+		console.log(results);
+		//alert(results);
+		}
+	rd++;
 	console.log(fighters[0].health, fighters[1].health);
-}
+};
 
 //old fight function
 /*function fight(){
 	var results;
 	//start match
 	//call player info & health
-		console.log(playerOne [0] + ":" + playerOne[1] + " *START* " + playerTwo[0] +":" + playerTwo[1]);
-	//alert(playerOne [0] + ":" + playerOne[1] + " *START* " + playerTwo[0] +":" + playerTwo[1]);
+		console.log(playerOne [0] + ":" + fighters[0] + " *START* " + playerTwo[0] +":" + fighters[1]);
+	//alert(playerOne [0] + ":" + fighters[0] + " *START* " + playerTwo[0] +":" + fighters[1]);
 	//loop through up to 10 rounds to play out the fight
 	for(var i = 0; i < 10; i++){
 		//random formula to generate damage
@@ -66,16 +86,16 @@ function fight(){
 		var f2 = Math.floor(Math.random() * (playerTwo[2] - minDam2) + minDam2);
 		
 		//generate damage on players
-		playerOne[1] = playerOne[1] - f1;
-		playerTwo[1] = playerTwo[1] - f2;
+		fighters[0] = fighters[0] - f1;
+		fighters[1] = fighters[1] - f2;
 		
 		//Update health of players
-		console.log("Round " + rd + ":" + playerOne [0] + ": " +playerOne[1] + " vs " + playerTwo[0] + ": " + playerTwo[1] + ".");
+		console.log("Round " + rd + ":" + playerOne [0] + ": " +fighters[0] + " vs " + playerTwo[0] + ": " + fighters[1] + ".");
 		
 		//check if either player has died
 		var results = winnerCheck();
 		if (results === "game on"){
-		//alert(playerOne [0] + ": " +playerOne[1] + " *ROUND " + rd + " OVER* " + playerTwo[0] + ": " + playerTwo[1] + ".");
+		//alert(playerOne [0] + ": " +fighters[0] + " *ROUND " + rd + " OVER* " + playerTwo[0] + ": " + fighters[1] + ".");
 		}else{
 		console.log(results);
 		//alert(results);
@@ -87,17 +107,17 @@ function fight(){
 // Winner Check Function
 function winnerCheck(){
 		var results = "game on";
-		if(playerOne[1] < 1 || playerTwo[1] < 1){
+		if(fighters[0] < 1 || fighters[1] < 1){
 			//check if BOTH players have died
-			if(playerOne[1] < 1 && playerTwo[1] < 1){
+			if(fighters[0] < 1 && fighters[1] < 1){
 			//output results of BOTH players death
 			results = "You Both Die!";
 			//check if player ONE has died and player TWO is alive
-			}else if (playerOne[1] < 1 && playerTwo[1] > 0){
+			}else if (fighters[0] < 1 && fighters[1] > 0){
 			//output results of player two win
 			results = playerTwo[0] + " Wins! " + playerOne [0] + " Dies!";
 			//check if player TWO has died and player ONE is alive
-			}else if (playerTwo[1] < 1 && playerOne[1] > 0){
+			}else if (fighters[1] < 1 && fighters[0] > 0){
 			//output results of player one win
 			results = playerOne [0] + " Wins! " + playerTwo[0] + " Dies!";
 			}
@@ -106,7 +126,7 @@ function winnerCheck(){
 
 }
 }
-})();
+//})();
 
 // Call Function
 fight();
